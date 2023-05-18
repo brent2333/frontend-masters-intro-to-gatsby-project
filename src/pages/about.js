ca9fe2abd73b65cd27adc289bc21db8f721002b5
Brent Lawson <brent.lawson@fleetcor.com>
2023-05-18 09:06:54 -0400

    starting course

---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default function AboutPage() {
+    return (
+        <div>About Page</div>
+    )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function IndexPage() {
+    return (
+        <main>
+            <h1>Hello FM</h1>
+        </main>
+    )
+}
